504419428c5e6f982d5ac820d2021196a98564a0
prodhanm <mustafizur.prodhan@gmail.com>
2025-05-01 13:56:34 -0400

    Both the sum() and the diff() works with recursion

--- math.cpp
+++ math.cpp
@@ -13,12 +13,26 @@ int sum(int num) {
 	}
 }
 
+int diff(int num) {
+	if (num == 0) {
+		return 0;
+	}
+	else if (num > 0) {
+		return num - sum(num - 1);
+	}
+	else {
+		return num - sum(num + 1);
+	}
+}
+
 void main()
 {
 	int num;
 	cout << "Enter a number: ";
 	cin >> num;
 	int total = sum(num);
+	int diffTotal = diff(num);
 	printf("Total: %d\n" , total);
+	printf("Difference: %d\n", diffTotal);
 }
 
